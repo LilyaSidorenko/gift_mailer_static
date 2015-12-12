b058ffd9e8edca95d10dcdf4ae91722023cbae09
Lilya <lilya.sidorenko1@gmail.com>
2015-12-13 01:29:40 +0200

    Create preview page

---
+++ src/js/jquery-clone.js
@@ -0,0 +1,57 @@
+'use strict';
+
+(function($) {
+    var defaults = {
+        "trigger": ".js-trigger-clone-tpl",
+        "insertInСontainer": ".js-tpl-container",
+        "remove": ".js-remove-tpl"
+    };
+
+    CloneTpl = function(options) {
+        this.options = $.extend({}, defaults, options || {});
+
+        this.$trigger = $(this.options.trigger);
+
+        this.cloneTplId = this.$trigger.data('copy-tpl');
+
+        this.clonedCallback = function(options) {};
+        this.removeCallback = function(options) {};
+
+        if (this.cloneTplId) {
+            this.init();
+        }
+    };
+
+    CloneTpl.prototype = {
+        init: function() {
+            this.initEvents();
+        },
+        initEvents: function() {
+            var self = this;
+
+            this.$trigger.on('click', function(e) {
+                e.preventDefault();
+
+                self.clone();
+
+                self.clonedCallback(self.options);
+
+
+            });
+
+            $('body').on('click', this.options.remove, function(e) {
+                e.preventDefault();
+
+                $(this)
+                    .parent()
+                    .remove();
+
+                self.removeCallback(self.options);
+            })
+        },
+        clone: function() {
+            $(this.options.insertInСontainer).append($(this.cloneTplId).html());
+
+        }
+    };
+})(jQuery);
